871d1dbc10805223e12ec8cadb8ddbaa0c5ea6aa
Z-BOT-V-2 IMASH <91847406+Z-BOT-V-2@users.noreply.github.com>
2021-10-18 03:21:53 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -5,24 +5,14 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: "Gives number link of the bot owner"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://www.linkpicture.com/q/imashz_1.png", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+*Creator number(Imash) : http://wa.me/94771323434?text=Hello%20Imash%20kohomda.%20*
 
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
-
-*For More Updates Subscribe The Channel ☝*
+*කම්මැලි නිසයි මේ ඔක්කොම🥵*
 `}) 
 
 }));
